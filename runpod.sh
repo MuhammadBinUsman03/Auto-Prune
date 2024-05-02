a85603994687cace04d2b031cfd6dba6299f47b6
Muhammad Bin Usman <muhammadbin.2003@gmail.com>
2024-05-02 13:17:27 +0500

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -5,37 +5,28 @@ huggingface-cli login --token $HF_TOKEN
 git clone https://github.com/MuhammadBinUsman03/PruneMe.git
 cd PruneMe
 pip install -r requirements.txt
-cd ./compute_block_similarity/
+# Drop the layers between [l_0, l_n] and merge the rest of layers
+cd ./slice_with_mergekit
+git clone https://github.com/cg123/mergekit.git
+cd mergekit
+pip install -e .
+cd ./../..
+
 
 #Calculate Layer Similarities
-output=$(python layer_similarity.py --model_path $MODEL_ID \
+cd ./compute_block_similarity/
+python layer_similarity.py --model_path $MODEL_ID \
                       --dataset $DATASET \
                       --dataset_column $TEXT_COLUMN \
                       --batch_size $BATCH \
                       --max_length $MAX_LENGTH \
                       --layers_to_skip $LAYERS_TO_SKIP \
                       --dataset_size $DATASET_SIZE \
-                      --dataset_subset $DATASET_SUBSET)
-last=$(echo "$output" | tail -n 1)
-#Parse the output to extract final layer number, base model dtype, and pruning layers l_0 ---> l_n
-final=$(echo "$last" | awk '{print $1}')
-dtype=$(echo "$last" | awk '{print $2}')
-l_0=$(echo "$last" | awk '{print $3}')
-l_n=$(echo "$last" | awk '{print $4}')
-
-
-# Drop the layers between [l_0, l_n] and merge the rest of layers
-cd ./../slice_with_mergekit
-git clone https://github.com/cg123/mergekit.git
-cd mergekit
-pip install -e .
-cd ..
-python MergeConfig.py $MODEL_ID $final $dtype $l_0 $l_n
-python merge_me.py
+                      --dataset_subset $DATASET_SUBSET
 
 # Upload to Hub
 if [ "$PUSH" == "True" ]; then   
-  cd merged
+  cd ./merged
   huggingface-cli upload $NEW_MODEL . .
 fi
 
